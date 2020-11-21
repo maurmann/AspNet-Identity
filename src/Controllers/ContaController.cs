d2b331dcacb314948e325432ba696a633f79461d
maurmann <adriano.maurmann@gmail.com>
2020-11-20 22:15:29 -0300

    update

--- src/Controllers/ContaController.cs
+++ src/Controllers/ContaController.cs
@@ -1,19 +1,33 @@
-﻿using AspNet_Identity.RequestModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using AspNet_Identity.Models;
+using AspNet_Identity.RequestModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Web.Http;
 
 namespace AspNet_Identity.Controllers
 {
+    [RoutePrefix("contas")]
     public class ContaController : ApiController
     {
 
         [HttpPost]
+        [Route("registrar")]
         public IHttpActionResult Registrar(ContaRegistrarRequest request)
         {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var dbContext = new IdentityDbContext<Usuario>("DefaultConnection");
+            var userStore = new UserStore<Usuario>(dbContext);
+            var userManager = new UserManager<Usuario>(userStore);
+
+            var novoUsuario = new Usuario();
+            novoUsuario.Email = request.Email;
+            novoUsuario.UserName = request.UserName;
+            novoUsuario.NomeCompleto = request.NomeCompleto;
+
+            userManager.Create(novoUsuario, request.Senha);
+
             return Ok();
         }
 
---
+++ src/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNet_Identity.Models
+{
+    public class Usuario : IdentityUser 
+    {
+        public string NomeCompleto { get; set; }
+    }
+}
